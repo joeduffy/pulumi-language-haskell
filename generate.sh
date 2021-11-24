a9be2b87595dffecf2f653324585eae50e2c5952
Joe Duffy <joe@pulumi.com>
2021-11-24 11:10:07 -0800

    Add initial Protobuf files
    The Pulumi runtime requires gRPC/Protobufs in order to communicate
    between the language host and the engine. This commits the generated
    files, using github.com/awakesecurity/gRPC-haskell's compile-proto-file
    utility. There is, sadly, some hacky sed'ding required because of
    the module layout that tool produces (with, apparently, no way to
    override).

---
+++ generate.sh
@@ -0,0 +1,45 @@
+set -e
+
+compile() {
+    echo "    - compile: include=$1, proto=$2, out=$3"
+    stack exec -- \
+        /nix/store/bapl3ldkcamaw4whkdks2bhzr5pbhh0r-ghc-8.10.3-with-packages/bin/compile-proto-file \
+        --includeDir $1 \
+        --proto $2 \
+        --out $3
+}
+
+fix_module_name() {
+    echo "    - fix: module=$1"
+    # Fix the declarations of the module.
+    sed -i.bak "s/module $1/module Pulumi\.Proto\.$1/g" ./lib/Pulumi/Proto/*.hs
+    # Fix the imports of the module.
+    sed -i.bak "s/import qualified $1/import qualified Pulumi\.Proto\.$1/g" ./lib/Pulumi/Proto/*.hs
+    # Fix the references to members of the module.
+    for d in $(grep -e '^newtype\|^data \([A-Za-z]*\)' lib/Pulumi/Proto/$1.hs | cut -d ' ' -f 2); do
+        sed -i.bak "s/$1\.$d/Pulumi\.Proto\.$1\.$d/g" ./lib/Pulumi/Proto/*.hs
+    done
+}
+
+# Compile the Pulumi protos.
+echo "Compiling Pulumi protos..."
+for f in $(ls ../proto/*.proto); do
+    compile ../proto $(basename $f) ./lib/Pulumi/Proto/
+done
+
+# HACK: Compile the Google protos -- we need to vendor these.
+echo "Compiling Google protos..."
+compile ../proto/google/protobuf empty.proto ./lib/Pulumi/Proto/
+sed -i.bak 's/Google\.Protobuf\./Pulumi\.Proto\./g' ./lib/Pulumi/Proto/*.hs
+
+# HACK: Fix up module names so they use qualified Pulumi.Proto names (and also not Google ones).
+echo "Fixing proto module names..."
+for f in $(ls ./lib/Pulumi/Proto/*.hs); do
+    bf=$(basename $f)
+    fix_module_name ${bf%.*}
+done
+# HACKHACK: Even worse, we can end up with double substitutions... ugh.
+sed -i.bak 's/Pulumi\.Proto\.Pulumi\.Proto\./Pulumi\.Proto\./g' ./lib/Pulumi/Proto/*.hs
+rm ./lib/Pulumi/Proto/*.hs.bak
+
+echo "DONE."
