2f38b4303bc0d78d42e8c91b2f7f963bd7b878d2
Joe Duffy <joe@pulumi.com>
2021-11-24 18:24:17 -0800

    Get the app to work

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,11 @@ import qualified Pulumi.Resource as Resource
 import qualified Pulumi.ResourceOptions as ResourceOptions
 
 main = do
-    bucketName <- rid $ bucket
+    bucketState <- state $ bucket
+    let bucketName = uid $ bucketState
     putStrLn bucketName
         where bucket = Resource.decl
-                       "aws:s3:Bucket"
+                       "aws:s3/bucket:Bucket"
                        "my-bucket"
                        Map.empty
                        ResourceOptions.empty
